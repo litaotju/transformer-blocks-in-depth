c9fd586bef7444acc37ad051f0e3380f619a32ba
Li Tao <litaotju@live.cn>
2023-02-07 23:50:38 +0800

    Make it self content

--- src/gpt.py
+++ src/gpt.py
@@ -1,8 +1,85 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
+
+import math
+import sys
 from typing import List
 from dataclasses import dataclass
-from decoder_with_kv_cache import DecoderLayer
+
+class MultiHeadSelfAtten(nn.Module):
+
+    def __init__(self, dim, head) -> None:
+        super().__init__()
+        self.dim = dim
+        self.head = head
+        self.qkv_proj = nn.Linear(dim, dim*3)
+        self.out_proj = nn.Linear(dim, dim)
+
+    # [B, S, D] -> [B, H, S, D/H]
+    def reshape(self, x):
+        return x.reshape(x.shape[0], -1, self.head, self.dim // self.head).transpose(1, 2)
+
+    def self_attn(self, q, k, v, mask):
+        # q: [B, H, S, D/H] or [B, H, 1, D/H]
+        # k, v: [B, H, S, D/H]
+        # out: [B, H, S, D/H] or [B, H, 1, D/H]
+        score = torch.matmul(q, k.transpose(-2, -1)) / (math.sqrt(self.dim)) 
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -1e3)
+        attn = score.softmax(dim=-1)
+        out = torch.matmul(attn, v)
+        return out
+
+    def forward(self, x, mask, past_keys=None, past_values=None):
+        # Non-cached state
+        #   x: [B, S, D], past_keys, past_values: None
+        #
+        # Cached:
+        #   past_keys and past_values are not None, they are of shape [B, H, S, D/H]
+        #   x: [B, 1, D], means "query" is only one token.
+        #   thus the hidden state passing to next step is only [B, 1, D]
+        q, k, v = self.qkv_proj(x).chunk(3, dim=-1)
+        q, k, v = self.reshape(q), self.reshape(k), self.reshape(v)
+
+        batch = q.shape[0]
+        if past_keys is not None:
+            # concat in time dimension
+            k = torch.cat((past_keys, k), dim=-2)
+            v = torch.cat((past_values, v), dim=-2)
+
+        out = self.self_attn(q, k, v, mask)
+        # merge heads: [B, H, S, D/H] -> [B, S, D]
+        out = out.transpose(1, 2).reshape(batch, -1, self.dim)
+
+        out = self.out_proj(out)
+        return out, k, v
+
+
+class DecoderLayer(nn.Module):
+
+    def __init__(self, dim, head, dim_ff) -> None:
+        super().__init__()
+        self.attn = MultiHeadSelfAtten(dim, head)
+        self.norm1 = nn.LayerNorm(dim)
+
+        self.norm2 = nn.LayerNorm(dim)
+        self.mlp_linear1 = nn.Linear(dim, dim_ff)
+        self.act= nn.GELU()
+        self.mlp_linear2 = nn.Linear(dim_ff, dim)
+
+    def forward(self, hidden, mask, past_keys=None, past_values=None):
+        '''
+         hidden: [B, S, D]
+         mask: [B, S, S]
+        '''
+        # gpt-2 and gpt-3 use layer norm before block, https://github.com/karpathy/minGPT
+        x = self.norm1(hidden)
+        x, k, v = self.attn(x, mask, past_keys, past_values)
+        x = hidden + x
+        x = x + self.mlp_linear2(self.act(self.mlp_linear1(self.norm2(x))))
+        return x, k, v
+
 
 @dataclass
 class GPTConfig:
@@ -18,11 +95,37 @@ class GPT(nn.Module):
 
     def __init__(self, config: GPTConfig) -> None:
         super().__init__()
-        self.decoders = nn.ModuleList([DecoderLayer(config.d_model, config.n_heads) for _ in range(config.n_layers)])
+        self.word_embedding = nn.Embedding(config.vocab_size, config.d_model)
+        self.pos_embedding = nn.Embedding(config.max_seq_len, config.d_model)
+        self.decoders = nn.ModuleList([DecoderLayer(config.d_model, config.n_heads, config.d_ff) for _ in range(config.n_layers)])
+        self.ln = nn.LayerNorm(config.d_model)
+        self.lm_head = nn.Linear(config.d_model, config.vocab_size, bias=False)
     
-    def forward(self, x, mask, past_keys: List[torch.Tensor], past_values: List[torch.Tensor]):
+    def forward(self, idx, mask, past_keys: List[torch.Tensor], past_values: List[torch.Tensor]):
+        '''
+        input shapes:
+            idx: [B, S]
+            mask: [B, S, S]
+            past_keys: [B, H, S, D/H]
+            past_values: [B, H, S, D/H]
+        output shapes:
+            logits: [B, 1]
+            past_keys: [B, H, S+1, D/H]
+            past_values: [B, H, S+1, D/H]
+        '''
         keys = []
-        values = [] 
+        values = []
+
+        if past_keys is None:
+            pos = torch.arange(idx.shape[1], dtype=torch.long).to(idx.device)
+        else:
+            # if past_keys is not None, the current token's position is the length of past keys
+            context_len = past_keys[0].shape[-2]
+            pos = torch.tensor(context_len, dtype=torch.long).to(idx.device)
+
+        x = self.word_embedding(idx)
+        x += self.pos_embedding(pos).unsqueeze(0)
+
         for i, decoder in enumerate(self.decoders):
             if past_keys is not None:
                 x, current_key, current_value = decoder(x, mask, past_keys[i], past_values[i])
@@ -30,31 +133,47 @@ class GPT(nn.Module):
                 x, current_key, current_value = decoder(x, mask, None, None)
             keys.append(current_key)
             values.append(current_value)
-        return x, keys, values
+        x = self.ln(x)
+        # only last token is used for prediction
+        logits = self.lm_head(x[:, -1:, :])
+        return logits, keys, values
+
+    def generate(self, prompt):
+        x = prompt
+        keys = None
+        values = None
+        iteration = 10
+
+        generated = []
+        for i in range(iteration):
+            # TODO: why the mask is [1, 1, 1, seq] here
+            # can the generation stage take no mask?
+            # since it's auto regressive, the current token should attend to all previous tokens
+            # seq = 32 + i + 1
+            # mask = torch.ones(1, 1, 1, seq).to('cuda:1')
+            mask = None
+            logits, keys, values = self(x, mask, keys, values)
+            logits = logits[:, -1:, :]
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution
+            x = torch.multinomial(probs[:,-1,:], num_samples=1)
+            generated.append(x)
+        return torch.cat(generated, dim=1)
+
+configs = {
+    'small' : GPTConfig(vocab_size=50257, n_layers=12, n_heads=12, d_model=768, d_ff=3072, dropout=0.1, max_seq_len=2048),
+    'gpt3' :GPTConfig(vocab_size=50257, n_layers=96, n_heads=96, d_model=12288, d_ff=49152, dropout=0.1, max_seq_len=2048)
+}
 
 if __name__ == "__main__":
+    config_name = sys.argv[1] if len(sys.argv) > 1 else 'small'
     device = 'cuda:1'
-    small = GPTConfig(vocab_size=5000, n_layers=12, n_heads=8, d_model=768, d_ff=3072, dropout=0.1, max_seq_len=1024)
-    # memic generation stage
-    gpt_small = GPT(small).to(device)
+
+    config = configs[config_name]
+    gpt_small = GPT(config).to(device)
 
     bs = 8
     context_len = 32
-    prompt =  torch.rand(bs, context_len, small.d_model).to(device)
-    x = prompt
-    keys = None
-    values = None
-
-    iteration = 10
-    for i in range(iteration):
-        # TODO: why the mask is [1, 1, 1, seq] here
-        # can the generation stage take no mask?
-        # since it's auto regressive, the current token should attend to all previous tokens
-        # seq = 32 + i + 1
-        # mask = torch.ones(1, 1, 1, seq).to('cuda:1')
-        print("iteration: ", i)
-        if keys is not None:
-            print("key shape", keys[0].shape)
-        mask = None
-        x, keys, values = gpt_small(x, mask, keys, values)
-        x = x[:, -1:, :]
+    prompt =  torch.randint(config.vocab_size, [bs, context_len]).to(device)
+    generated = gpt_small.generate(prompt)
+    print("generated shape:", generated.shape)
