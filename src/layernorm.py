55f274e3fede5bb6c570bcf929285ccf1d8d6fed
Li Tao <litaotju@live.cn>
2023-02-11 11:28:16 +0800

    Delete dead code

--- src/layernorm.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-norm = nn.LayerNorm(512).to('cuda')
-
-# x = norm(torch.rand(1, 32, 512).to('cuda'))
-
-import tops
-tops.benchmark(norm, (torch.rand(1, 32, 512).to('cuda'),), verbose=True, logdir="./logs/layernorm-1")
